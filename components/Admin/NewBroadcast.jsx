bfcd468ce67442da2c73fbd0b7d109464c9b134e
Ken Schenke <kschenke@datafreight.com>
2018-12-10 09:54:01 -0600

    Show status message and disable ok button when sending new broadcast

--- components/Admin/NewBroadcast.jsx
+++ components/Admin/NewBroadcast.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { mapNewBroadcastProps, mapNewBroadcastDispatch } from '../maps/Admin/NewBroadcast.map';
 import { connect } from 'react-redux';
-import { DuxOkDialog, DuxProgressDialog, ProgressBarBS4 } from 'duxpanel';
+import { DuxOkDialog, ProgressBarBS4 } from 'duxpanel';
 import { NewBroadcastMessage } from './NewBroadcastMessage.jsx';
 import { NewBroadcastRecipients } from './NewBroadcastRecipients.jsx';
 import { NewBroadcastSchedule } from './NewBroadcastSchedule.jsx';
@@ -32,68 +32,52 @@ class NewBroadcastUi extends React.Component {
 
     render() {
         return (
-            <div>
-                <DuxOkDialog show={true}
-                             title="New Broadcast"
-                             okClassName="btn btn-primary"
-                             cancelClassName="btn btn-warning"
-                             onOk={this.props.okClicked}
-                             onCancel={this.props.cancelClicked}
-                             showCancel={true}
-                             shouldClose={this.isBroadcastValid}
-                             width={{
-                                 xs: '95%',
-                                 md: '50%'
-                             }}
-                >
-                    <ul className="nav nav-tabs">
-                        <li className="nav-item">
-                            <a href="#" onClick={e => this.tabClicked(e,'message')} className={'nav-link' + (this.state.tabName==='message' ? ' active' : '')}>Message</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" onClick={e => this.tabClicked(e,'recipients')} className={'nav-link' + (this.state.tabName==='recipients' ? ' active' : '')}>Recipients</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" onClick={e => this.tabClicked(e,'schedule')} className={'nav-link' + (this.state.tabName==='schedule' ? ' active' : '')}>Schedule</a>
-                        </li>
-                        <li className="nav-item d-none d-lg-block d-xl-block">
-                            <a href="#" onClick={e => this.tabClicked(e,'attachment')} className={'nav-link' + (this.state.tabName==='attachment' ? ' active' : '')}>Attachment</a>
-                        </li>
-                    </ul>
+            <DuxOkDialog show={true}
+                         title="New Broadcast"
+                         okClassName="btn btn-primary"
+                         cancelClassName="btn btn-warning"
+                         onOk={this.props.okClicked}
+                         onCancel={this.props.cancelClicked}
+                         showCancel={true}
+                         shouldClose={this.isBroadcastValid}
+                         statusMsg={this.props.saving ? 'Saving new broadcast.' : ''}
+                         statusMsgClassName="ml-3 text-muted"
+                         okDisabled={this.props.saving}
+                         width={{
+                             xs: '95%',
+                             md: '50%'
+                         }}
+            >
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <a href="#" onClick={e => this.tabClicked(e,'message')} className={'nav-link' + (this.state.tabName==='message' ? ' active' : '')}>Message</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" onClick={e => this.tabClicked(e,'recipients')} className={'nav-link' + (this.state.tabName==='recipients' ? ' active' : '')}>Recipients</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" onClick={e => this.tabClicked(e,'schedule')} className={'nav-link' + (this.state.tabName==='schedule' ? ' active' : '')}>Schedule</a>
+                    </li>
+                    <li className="nav-item d-none d-lg-block d-xl-block">
+                        <a href="#" onClick={e => this.tabClicked(e,'attachment')} className={'nav-link' + (this.state.tabName==='attachment' ? ' active' : '')}>Attachment</a>
+                    </li>
+                </ul>
 
-                    <div className={'newbroadcast-tab' + (this.state.tabName==='message' ? '' : ' d-none')}>
-                        <NewBroadcastMessage/>
-                    </div>
-                    <div className={'newbroadcast-tab' + (this.state.tabName==='recipients' ? '' : ' d-none')}>
-                        <NewBroadcastRecipients/>
-                    </div>
-                    <div className={'newbroadcast-tab' + (this.state.tabName==='schedule' ? '' : ' d-none')}>
-                        <NewBroadcastSchedule/>
-                    </div>
-                    <div className={'newbroadcast-tab' + (this.state.tabName==='attachment' ? '' : ' d-none')}>
-                        <NewBroadcastAttachment/>
-                    </div>
+                <div className={'newbroadcast-tab' + (this.state.tabName==='message' ? '' : ' d-none')}>
+                    <NewBroadcastMessage/>
+                </div>
+                <div className={'newbroadcast-tab' + (this.state.tabName==='recipients' ? '' : ' d-none')}>
+                    <NewBroadcastRecipients/>
+                </div>
+                <div className={'newbroadcast-tab' + (this.state.tabName==='schedule' ? '' : ' d-none')}>
+                    <NewBroadcastSchedule/>
+                </div>
+                <div className={'newbroadcast-tab' + (this.state.tabName==='attachment' ? '' : ' d-none')}>
+                    <NewBroadcastAttachment/>
+                </div>
 
-                    <small className={'text-danger' + (this.state.validMsgVisible ? '' : ' invisible')} dangerouslySetInnerHTML={{__html:this.props.validMsg}}></small>
-                </DuxOkDialog>
-                <DuxProgressDialog
-                    show={this.props.saving}
-                    title="Please Wait"
-                    min={0}
-                    max={1}
-                    value={1}
-                    progressComponent={this.progressBar}
-                    center={true}
-                    top={100}
-                    width={500}
-                    slideInFrom={null}
-                    slideOutTo={null}
-                >
-                    <p>
-                        Saving new broadcast.  Please wait.
-                    </p>
-                </DuxProgressDialog>
-            </div>
+                <small className={'text-danger' + (this.state.validMsgVisible ? '' : ' invisible')} dangerouslySetInnerHTML={{__html:this.props.validMsg}}></small>
+            </DuxOkDialog>
         );
     }
 }
